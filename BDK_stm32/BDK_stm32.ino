320dcb70638e8efe4036a1b022428314c8f11cb4
thanhhung0112 <thanhhungthh987@gmail.com>
2022-11-02 03:06:18 +0700

    update code

--- BDK_stm32/BDK_stm32.ino
+++ BDK_stm32/BDK_stm32.ino
@@ -1,19 +1,19 @@
 void compute_pwm(int theta, float mcu, bool flag, double & pwm1, double & pwm2) {
   double loss;
-  loss = 1 + (theta - mcu) / 90;
+  loss = 1 + abs(theta - mcu) / 90; // loss has the value from 1 to 2 correspondence 0 to 90 (error between theta and mcu)
   double ratio;
   ratio = 1 + pow(loss, 3) * log(loss);
-  if (loss > 0){
-    pwm1 = pwm1/ratio;
+  if (theta - mcu > 0){
+    pwm1 = pwm1/ratio; // turn the left
   }
-  else if (loss < 0){
-    pwm2 = pwm2/ratio;
+  else if (theta - mcu < 0){
+    pwm2 = pwm2/ratio; // turn the right
   }
 }
 
 void setup(){
-  double pwm1=80;
-  double pwm2=80;
+  double pwm1=80; // pwm pulse for the left motor
+  double pwm2=80; // pwm pulse for the right motor
   int theta; // get the specified value in the specified stage such as 0 degree in stage 1, 90 degree in stage 2, etc
   float mcu; // the value which the mcu sensor returns
   bool flag; // set to 1 when encountering obstacle

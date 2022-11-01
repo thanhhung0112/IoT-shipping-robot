2595064bc7ab48659442299b58efed799cab828c
thanhhung0112 <thanhhungthh987@gmail.com>
2022-11-02 03:34:05 +0700

    update code

--- BDK_stm32/BDK_stm32.ino
+++ BDK_stm32/BDK_stm32.ino
@@ -12,14 +12,15 @@ void compute_pwm(int theta, float mcu, bool flag, double & pwm1, double & pwm2) 
 }
 
 void setup(){
-  double pwm1=80; // pwm pulse for the left motor
-  double pwm2=80; // pwm pulse for the right motor
+  Serial.begin(9600);
+}
+void loop(){
   int theta; // get the specified value in the specified stage such as 0 degree in stage 1, 90 degree in stage 2, etc
   float mcu; // the value which the mcu sensor returns
   bool flag; // set to 1 when encountering obstacle
+  double pwm1=80; // pwm pulse for the left motor
+  double pwm2=80; // pwm pulse for the right motor
   compute_pwm(theta, mcu, flag, pwm1, pwm2);
-}
-void loop(){
   if (flag != 1) {
     // generate pulse
     // flag == 1 when the robot encountered any obstacle (workers), then do not generate pulse until the flag bool variable becomes 0 again

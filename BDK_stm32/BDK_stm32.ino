fada24447307ad7f8606928c5f0af2a21a0d04b9
thanhhung0112 <thanhhungthh987@gmail.com>
2022-11-02 21:34:59 +0700

    update

--- BDK_stm32/BDK_stm32.ino
+++ BDK_stm32/BDK_stm32.ino
@@ -1,31 +1,37 @@
-const int pwm1; // pwm pulse for the left motor
-const int pwm2; // pwm pulse for the right motor
+const int pwm1=PA1;
+const int pwm2=PA2;
+int i=0;
+int j=0;
+int pulse_val1;
+int pulse_val2;
 const float mcu; // the value which the mcu sensor returns
-const bool flag; // set to 1 when encountering obstacle
 const int theta; // get the specified value in the specified stage such as 0 degree in stage 1, 90 degree in stage 2, etc
 
-void compute_pwm(int theta, float mcu, bool flag, double & pwm1, double & pwm2) {
-  double loss;
-  loss = 1 + abs(theta - mcu) / 90; // loss has the value from 1 to 2 correspondence 0 to 90 (error between theta and mcu)
-  double ratio;
-  ratio = 1 + pow(loss, 3) * log(loss);
-  if (theta - mcu > 0){
-    pwm1 = pwm1/ratio; // turn the left
-  }
-  else if (theta - mcu < 0){
-    pwm2 = pwm2/ratio; // turn the right
-  }
+void compute_ratio(int theta, float mcu) {
+	double loss;
+	loss = 1 + abs(theta - mcu) / 90; // loss has the value from 1 to 2 correspondence 0 to 90 (error between theta and mcu)
+	double ratio;
+	ratio = pow(loss, 3) * log(loss);
+	return ratio;
 }
 
-void setup(){
-  Serial.begin(9600);
-  pinMode(pwm1, PWM);
-  pinMode(pwm2, PWM);
+void setup() {
+	pinMode(pwm2, PWM);
+	pinMode(pwm1, PWM);
+	Serial.begin(9600);
 }
-void loop(){
-  compute_pwm(theta, mcu, flag, pwm1, pwm2);
-  if (flag != 1) {
-    // generate pulse
-    // flag == 1 when the robot encountered any obstacle (workers), then do not generate pulse until the flag bool variable becomes 0 again
-  }
+
+void loop() {
+	ratio = compute_ratio(theta, mcu);
+	pulse_val1 = (int) (i/(1 + (1 + abs(theta-mcu+0.0001) / (theta-mcu+0.0001))*ratio/2));
+	pulse_val2 = (int) (j/(1 + (1 - abs(theta-mcu-0.0001) / (theta-mcu-0.0001))*ratio/2));
+	pwmWrite(pwm1, pulse_val1);
+	pwmWrite(pwm2, pulse_val2);
+	digitalWrite(PA0, LOW);
+	digitalWrite(PA3, LOW);
+	i = i + 1000;
+	j = j + 1000;
+	delay(100);
+	if (i > 65535) i = 65535;
+	if (j > 65535) j = 65535;
 }

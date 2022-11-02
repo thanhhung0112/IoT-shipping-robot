8e0986f73f227ba45f4b5d810c102aedfd4e22b3
thanhhung0112 <thanhhungthh987@gmail.com>
2022-11-02 18:31:23 +0700

    update code

--- BDK_stm32/BDK_stm32.ino
+++ BDK_stm32/BDK_stm32.ino
@@ -1,3 +1,9 @@
+const int pwm1; // pwm pulse for the left motor
+const int pwm2; // pwm pulse for the right motor
+const float mcu; // the value which the mcu sensor returns
+const bool flag; // set to 1 when encountering obstacle
+const int theta; // get the specified value in the specified stage such as 0 degree in stage 1, 90 degree in stage 2, etc
+
 void compute_pwm(int theta, float mcu, bool flag, double & pwm1, double & pwm2) {
   double loss;
   loss = 1 + abs(theta - mcu) / 90; // loss has the value from 1 to 2 correspondence 0 to 90 (error between theta and mcu)
@@ -13,13 +19,10 @@ void compute_pwm(int theta, float mcu, bool flag, double & pwm1, double & pwm2) 
 
 void setup(){
   Serial.begin(9600);
+  pinMode(pwm1, PWM);
+  pinMode(pwm2, PWM);
 }
 void loop(){
-  int theta; // get the specified value in the specified stage such as 0 degree in stage 1, 90 degree in stage 2, etc
-  float mcu; // the value which the mcu sensor returns
-  bool flag; // set to 1 when encountering obstacle
-  double pwm1=80; // pwm pulse for the left motor
-  double pwm2=80; // pwm pulse for the right motor
   compute_pwm(theta, mcu, flag, pwm1, pwm2);
   if (flag != 1) {
     // generate pulse

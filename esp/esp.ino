92156efb978e7da1dd9d0c16ceab33991508069e
thanhhung0112 <thanhhungthh987@gmail.com>
2022-12-04 18:36:03 +0700

    update

--- esp/esp.ino
+++ esp/esp.ino
@@ -16,64 +16,143 @@ char* ssid = "Nguyen Hieu";
 char* pass = "0359341471";
 
 String DataESP = "";
-String xuatphat="";
-String hang="";
-String vatcan="";
+String hang = "";
+String vatcan = "";
+String xuatphat;
+String state;
+String S;
+String D;
 String DataSend;
 long last;
 void Read_STM(void);
+
+int buttonV1;
+
+
 void setup() {
-  
   Serial.begin(9600);
-  ESPSerial.begin(9600);
+  ESPSerial.begin(19200);
   Blynk.begin(auth, ssid, pass);
 }
 
 void loop() {
   Blynk.run();
   Read_STM();
-  
-  
-  if (millis()-last>=1500){
+
+  /*if (millis()-last>=1500){
     send_Data();
     last=millis();
-    }
+    }*/
 }
-void kiemtrahang(void){
-  if (hang=="1"){
+BLYNK_CONNECTED() {
+  Blynk.syncAll();
+}
+BLYNK_WRITE(V1) {
+  int buttonV1 = param.asInt();
+  if (buttonV1 == 0) {
+    xuatphat = "0";
+  }
+  else {
+    xuatphat = "1";
+  }
+}
+BLYNK_WRITE(V12) {
+  int buttonV12 = param.asInt();
+  if (buttonV12 == 1) {
+    S = "S1";
+    Blynk.virtualWrite(V13, LOW);
+    Blynk.virtualWrite(V14, LOW);
+    Blynk.virtualWrite(V15, LOW);
+  }
+}
+BLYNK_WRITE(V13) {
+  int buttonV13 = param.asInt();
+  if (buttonV13 == 1) {
+    S = "S2";
+    Blynk.virtualWrite(V12, LOW);
+    Blynk.virtualWrite(V14, LOW);
+    Blynk.virtualWrite(V15, LOW);
+  }
+}
+BLYNK_WRITE(V14) {
+  int buttonV14 = param.asInt();
+  if (buttonV14 == 1) {
+    S = "S3";
+    Blynk.virtualWrite(V13, LOW);
+    Blynk.virtualWrite(V12, LOW);
+    Blynk.virtualWrite(V15, LOW);
+  }
+}
+BLYNK_WRITE(V15) {
+  int buttonV15 = param.asInt();
+  if (buttonV15 == 1) {
+    S = "S4";
+    Blynk.virtualWrite(V13, LOW);
+    Blynk.virtualWrite(V14, LOW);
+    Blynk.virtualWrite(V12, LOW);
+  }
+}
+BLYNK_WRITE(V16) {
+  int buttonV16 = param.asInt();
+  if (buttonV16 == 1) {
+    D = "DA";
+    Blynk.virtualWrite(V17, LOW);
+    Blynk.virtualWrite(V18, LOW);
+  }
+}
+BLYNK_WRITE(V17) {
+  int buttonV17 = param.asInt();
+  if (buttonV17 == 1) {
+    D = "DB";
+    Blynk.virtualWrite(V16, LOW);
+    Blynk.virtualWrite(V18, LOW);
+  }
+}
+BLYNK_WRITE(V18) {
+  int buttonV18 = param.asInt();
+  if (buttonV18 == 1) {
+    D = "DC";
+    Blynk.virtualWrite(V16, LOW);
+    Blynk.virtualWrite(V17, LOW);
+  }
+}
+void kiemtrahang(void) {
+  if (hang == "1") {
     Serial.println("Co hang");
     Blynk.virtualWrite(V0, HIGH);
-    }
-  if (hang=="0") {
+  }
+  if (hang == "0") {
     Serial.println("Khong co hang");
     Blynk.virtualWrite(V0, LOW);
-    }  
   }
-void kiemtravatcan(void){
-  if (vatcan=="1"){
+}
+void kiemtravatcan(void) {
+  if (vatcan == "1") {
     Serial.println("Co vat can");
     Blynk.virtualWrite(V11, HIGH);
-    }
-  if (vatcan=="0") {
+  }
+  if (vatcan == "0") {
     Serial.println("Khong co vat can");
     Blynk.virtualWrite(V11, LOW);
-    }
   }
-void send_Data(void){
-  Json(String(xuatphat));
-  }
-void Json(String thamso1){
-  DataSend="";
-  DataSend= String(thamso1);
+}
+void send_Data(void) {
+  Json(String(xuatphat) , String(S) , String(D));
+}
+void Json(String thamso1, String thamso2, String thamso3 ) {
+  DataSend = "";
+  DataSend = "{\"xuatphat\":\"" + thamso1 + "\"," +
+             "\"S\":\"" + thamso2 + "\"," +
+             "\"D\":\"" + thamso3 + "\"}";
   ESPSerial.println(DataSend);
   ESPSerial.flush();
-  } 
-void Read_STM(void){
-  while (ESPSerial.available()){
+}
+void Read_STM(void) {
+  while (ESPSerial.available()) {
     char InChar = (char)ESPSerial.read();
-    if (InChar != '\n'){
+    if (InChar != '\n') {
       DataESP += InChar;
-      }
+    }
     else {
       Serial.print("DataESP:");
       Serial.println(DataESP);
@@ -81,28 +160,31 @@ void Read_STM(void){
       kiemtrahang();
       kiemtravatcan();
       Serial.println("---------------");
-      DataESP="";
-      }   
+      DataESP = "";
+      delay(5);
+      send_Data();
     }
   }
-void XuliJson(String DataESP){
-  const size_t capacity = JSON_OBJECT_SIZE(2)+256;
+}
+void XuliJson(String DataESP) {
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 256;
   DynamicJsonDocument JSON(capacity);
   deserializeJson(JSON, DataESP);
 
-  if (JSON["hang"]=="0"){
+  if (JSON["hang"] == "0") {
     hang = "0";
-    }
-  if (JSON["hang"]=="1"){
-    hang = "1";
-    }
-  if (JSON["vatcan"]=="0"){
-    vatcan = "0";
-    }
-  if (JSON["vatcan"]=="1"){
-    vatcan = "1";
-    }
-   Serial.print("hang:"); Serial.println(hang);
-   Serial.print("vatcan:"); Serial.println(vatcan);
-   
   }
+  if (JSON["hang"] == "1") {
+    hang = "1";
+  }
+  if (JSON["vatcan"] == "0") {
+    vatcan = "0";   
+  }
+  if (JSON["vatcan"] == "1") {
+    vatcan = "1";
+  }
+
+  Serial.print("hang:"); Serial.println(hang);
+  Serial.print("vatcan:"); Serial.println(vatcan);
+
+}
--- stm/stm.ino
+++ stm/stm.ino
@@ -1,111 +1,277 @@
+#include <ArduinoJson.h>
+
+String S;
+String D;
+
+float arr1[9] = {4000, 2000, 4000, 500, 4000, 1700, 3000, 1000, 500};
+int state = -1;
 String DataSend = "";
 String DataSTM = "";
-long last=0;
+long last = 0;
+long lastT = 0;
+long lastP = 0;
+long last0 = 0;
+long lastC = 0;
+long vT;
+int vTrai = 0;
+int pwmT = 40000;
+long T = 0;
+long T1 = 0;
+long vP;
+int vPhai = 0;
+int pwmP = 41250;
+long P = 0;
+long P1 = 0;
+bool rt = 0;
+bool rp = 0;
 
-String hang="";
-String vatcan="";
+String xuatphat;
+String hang = "";
+String vatcan = "";
 void read_ESP8266(void);
 void send_Data(void);
 void kiemtrahang(void);
 void kiemtravatcan(void);
+void chondiemden(void);
+
 
 #define hongngoai PA15
 #define trig PB8
-#define echo PB9
+#define echo PB7
+#define ecdT PA6
+#define ecdP PA4
+
+
 
 int khoangcach;
-const int pwm1=PA1;
-const int pwm2=PA2;
-int i=65535;
-int j=30000;
+const int pwm1 = PA0;
+const int pwm2 = PA3;
+int i = 0;
+int j = 0;
 
 void setup()
-{ 
-	pinMode(pwm2,PWM);
-	pinMode(pwm1,PWM);
-	pinMode(hongngoai, INPUT_PULLUP);
-	pinMode(trig, OUTPUT);
-	pinMode(echo, INPUT);
-	Serial1.begin(9600);
+{
+  pinMode(ecdT, INPUT_PULLUP);
+  pinMode(ecdP, INPUT_PULLUP);
+  pinMode(PA1, OUTPUT);
+  pinMode(PA2, OUTPUT);
+  pinMode(pwm2, PWM);
+  pinMode(pwm1, PWM);
+  pinMode(hongngoai, INPUT_PULLUP);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial1.begin(19200);
+  Serial.begin(9600);
 }
 
 void loop()
-{	if ((millis()<2000) || (millis()>3000 && millis()<5000)) {                                                                                                 
-		pwmWrite(pwm1, i);
-		pwmWrite(pwm2, i);
-		digitalWrite(PA0, LOW);
-		digitalWrite(PA3, LOW);
-	}
-	else if ((millis()>2000) && (millis()<3000)) {
-		pwmWrite(pwm1, i);
-		pwmWrite(pwm2, i-j);
-		digitalWrite(PA0, LOW);
-		digitalWrite(PA3, LOW);
-		delay(1000);
-	}
-	else {
-		pwmWrite(pwm1,0);
-		pwmWrite(pwm2,0);
-	} 
-	/* i = i + 10000; */
-	/* if (i>65535) i=65535; */
-	/* j = j - 1000; */
-	/* if (j<0) j=0; */
+{
+  read_ESP8266();
+  chondiemden();
+  if (rt == 1) {
+    pwmWrite(pwm1, 0);
+    digitalWrite(PA1, LOW);
+    pwmWrite(pwm2, pwmP);
+    digitalWrite(PA2, LOW);
+    delay(850);
+    rt = 0;
+    state++;
+    last0 = millis();
+  }
+  else if (rp == 1) {
+    pwmWrite(pwm1, pwmT);
+    digitalWrite(PA1, LOW);
+    pwmWrite(pwm2, 0);
+    digitalWrite(PA2, LOW);
+    delay(850);
+    rp = 0;
+    state++;
+    last0 = millis();
+  }
+  while (state == -1) {
+    pwmWrite(pwm1, 0);
+    digitalWrite(PA1, LOW);
+    pwmWrite(pwm2, 0 );
+    digitalWrite(PA2, LOW);
+    kiemtrahang();
+    kiemtravatcan();
+    /*Serial.print("state:");
+    Serial.println(state);*/
+    if (millis() - last >= 500) {
+      send_Data();
+      last = millis();
+    }
+    read_ESP8266();
+    if (xuatphat == "1") state = 0;
+    last0 = millis();
+  }
+  if ((rt == 0) && (rp == 0) && (state < 9)) {
+    pwmWrite(pwm1, pwmT);
+    digitalWrite(PA1, LOW);
+    pwmWrite(pwm2, pwmP);
+    digitalWrite(PA2, LOW);
+    kiemtravatcan();
+    lastC = millis();
+    while (khoangcach <= 20) {
+      pwmWrite(pwm1, 0);
+      pwmWrite(pwm2, 0);
+      kiemtrahang();
+      kiemtravatcan();
+      if (millis() - last >= 500) {
+        send_Data();
+        last = millis();
+      }
 
-	/* read_ESP8266(); */
-	/* kiemtrahang(); */
-	/* kiemtravatcan(); */
-	/* if (millis()-last>=1500) { */
-	/* 	send_Data(); */
-	/* 	last=millis(); */
-	/* } */
+    }
+    
+    last0 = last0 + (millis() - lastC);
+    
+    
+  }
+  if (abs(millis() - last0 - arr1[state]) <= 10) {
+    if (state == 0) rt = 1;
+    else if (state == 1) rt = 1;
+    else if (state == 2) rt = 1;
+    else if (state == 3) rt = 1;
+    else if (state == 4) rp = 1;
+    else if (state == 5) rp = 1;
+    else if (state == 6) {
+      while (digitalRead(hongngoai) == 0) {
+        pwmWrite(pwm1, 0);
+        digitalWrite(PA1, LOW);
+        pwmWrite(pwm2, 0);
+        digitalWrite(PA2, LOW);
+        kiemtrahang();
+        kiemtravatcan();
+        if (millis() - last >= 500) {
+          send_Data();
+          last = millis();
+        }
+      }
+      state++;
+      last0 = millis();
+    }
+    else if (state == 7) rt = 1;
+    else if (state == 8) rt = 1;
+  }
+  if (state == 9) {
+    pwmWrite(pwm1, 0);
+    pwmWrite(pwm2, 0);
+  }
+  kiemtrahang();
+  kiemtravatcan();
+  /*Serial.print("state:");
+  Serial.println(state);  
+  Serial.println(abs(millis() - last0));
+  Serial.print("lastC:");
+  Serial.println(lastC);
+  Serial.println(millis() - lastC);*/
+  if (millis() - last >= 500) {
+    send_Data();
+    last = millis();
+  }
+}
+void chondiemden(void) {
+  if (S = "S1") {arr1[6]=4000; arr1[7]=1000;}
+  else if (S = "S2") {arr1[6]=4000; arr1[7]=1000;}
+  else if (S = "S3") {arr1[6]=4000; arr1[7]=1000;}
+  else if (S = "S4") {arr1[6]=4000; arr1[7]=1000;}
+  if (D = "DA") {arr1[2]=4000; arr1[4]=1000;}
+  else if (D = "DA") {arr1[2]=4000; arr1[4]=1000;}
+  else if (D = "DA") {arr1[2]=4000; arr1[4]=1000;}
+}
+void kiemtrahang(void) {
+  if (digitalRead(hongngoai) == 0) {
+    hang = "1";
+  }
+  if (digitalRead(hongngoai) == 1) {
+    hang = "0";
+  }
 }
 
-void kiemtrahang(void){
-	if (digitalRead(hongngoai)==0){
-		hang="1";
-	}
-	if (digitalRead(hongngoai)==1) {
-		hang="0";
-	}  
+void kiemtravatcan(void) {
+  unsigned long duration;
+  digitalWrite(trig, 0);
+  delayMicroseconds(2);
+  digitalWrite(trig, 1);
+  delayMicroseconds(5);
+  digitalWrite(trig, 0);
+  duration = pulseIn(echo, HIGH);
+  khoangcach = int((duration / 2) / 29.412);
+  if (khoangcach <= 20) {
+    vatcan = "1";
+  }
+  else {
+    vatcan = "0";
+  }
+
+}
+void send_Data(void) {
+  Json(String(hang), String(vatcan));
 }
 
-void kiemtravatcan(void){
-	unsigned long duration;
-	digitalWrite(trig,0);
-	delayMicroseconds(2);
-	digitalWrite(trig,1);
-	delayMicroseconds(5);
-	digitalWrite(trig,0);
-	duration = pulseIn(echo,HIGH);
-	khoangcach = int((duration/2)/29.412);
-	if (khoangcach <= 20){
-		vatcan= "1";
-	}
-	else {
-		vatcan="0";
-	}
-	delay(200);
+void Json(String thamso1, String thamso2) {
+  DataSend = "";
+  DataSend = "{\"hang\":\"" + thamso1 + "\"," +
+             "\"vatcan\":\"" + thamso2 + "\"}";
+  Serial1.println(DataSend);
+  Serial.println(DataSend);
+  Serial1.flush();
 }
 
-void send_Data(void){
-	Json(String(hang), String(vatcan));
+void read_ESP8266(void) {
+  while (Serial1.available()) {
+    char InChar = (char)Serial1.read();
+    if (InChar != '\n') {
+      DataSTM += InChar;
+    }
+    else {
+      Serial.print("DataSTM:");
+      Serial.println(DataSTM);
+      XuliJson(DataSTM);
+      Serial.println("---------------");
+      DataSTM = "";
+    }
+  }
 }
 
-void Json(String thamso1, String thamso2){
-	DataSend="";
-	DataSend= "{\"hang\":\"" + String(thamso1) + "\"," +
-			 "\"vatcan\":\"" + String (thamso2)+ "\"}";
-	Serial1.println(DataSend);
-	Serial1.flush();
-}  
+void XuliJson(String DataSTM) {
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 256;
+  DynamicJsonDocument JSON(capacity);
+  deserializeJson(JSON, DataSTM);
 
-void read_ESP8266(void){
-	while (Serial1.available()){
-		char InChar = (char)Serial1.read();
+  if (JSON["xuatphat"] == "0") {
+    xuatphat = "0";
+  }
+  if (JSON["xuatphat"] == "1") {
+    xuatphat = "1";
+  }
+  if (JSON["S"] == "S1") {
+    S = "S1";
+  }
+  if (JSON["S"] == "S2") {
+    S = "S2";
+  }
+  if (JSON["S"] == "S3") {
+    S = "S3";
+  }
+  if (JSON["S"] == "S4") {
+    S = "S4";
+  }
+  if (JSON["S"] == "S4") {
+    S = "S4";
+  }
+  if (JSON["D"] == "DA") {
+    D = "DA";
+  }
+  if (JSON["D"] == "DB") {
+    D = "DB";
+  }
+  if (JSON["D"] == "DC") {
+    D = "DC";
+  }
 
-		if (InChar != '\n'){
-			DataSTM += InChar;
-		}
-	}
+  Serial.print("xuatphat:"); Serial.println(xuatphat);
+  Serial.print("S:"); Serial.println(S);
+  Serial.print("D:"); Serial.println(D);
 }
